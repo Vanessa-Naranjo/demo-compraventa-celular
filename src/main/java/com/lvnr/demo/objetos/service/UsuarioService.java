35329802ed432e85d2940a1e0c6d051152df7ca3
Vanessa Naranjo <vanelau_94@hotmail.com>
2025-08-23 19:51:47 -0500

    Subo punto 1(ii) getAll

--- src/main/java/com/lvnr/demo/objetos/service/UsuarioService.java
+++ src/main/java/com/lvnr/demo/objetos/service/UsuarioService.java
@@ -1,5 +1,22 @@
 package com.lvnr.demo.objetos.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.lvnr.demo.objetos.dto.UsuarioDto;
+
 public class UsuarioService {
+
+	private List<UsuarioDto> usuarios;
+
+	public UsuarioService () {
+		
+		usuarios=new ArrayList<>();
+		
+	}
+	
+	public List<UsuarioDto> getAll () {
+		return usuarios;
+	}
 
 }
