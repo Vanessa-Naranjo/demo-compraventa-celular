84084bd0eaa5c3f231cb8a53c68b8f4eb9b81d5e
Vanessa Naranjo <vanelau_94@hotmail.com>
2025-08-26 10:24:19 -0500

    Modificaciones 26/08/2025

--- src/main/java/com/lvnr/demo/objetos/controller/CompraVentaController.java
+++ src/main/java/com/lvnr/demo/objetos/controller/CompraVentaController.java
@@ -13,21 +13,24 @@ public class CompraVentaController {
 
 	public static void main(String[] args) {
 
-		PersonaDto personaDto = new PersonaDto();
 		CelularDto celularDto = new CelularDto();
-		UsuarioDto usuarioDto = new UsuarioDto();
+		celularDto.setMarca("Samsung");
+		celularDto.setModelo("Galaxy20");
+		celularDto.setValor(1500000);
 
 		System.out.println("****Crear Usuario****");
 		UsuarioService usuarioService = new UsuarioService();
 		usuarioService.createUsuario("12345R", "Carlos Rodriguez");
 		usuarioService.createUsuario("23456M", "Laura Martinez");
 		usuarioService.createUsuario("78910M", "Gloria Morales");
+		UsuarioDto usuarioDto = usuarioService.getByCodigo("12345R");
 
 		System.out.println("****Crear Persona*****");
 		PersonaService personaService = new PersonaService();
 		personaService.createPersona("634829", "Elena Corredor");
 		personaService.createPersona("983743", "Eduardo Rodriguez");
 		personaService.createPersona("103184", "Angie Zarate");
+		PersonaDto personaDto = personaService.getByDocumento("634829");
 
 		System.out.println("****Crear Celular*****");
 		CelularService celularService = new CelularService();
@@ -38,10 +41,13 @@ public class CompraVentaController {
 		System.out.println("****Crear Compra******");
 		CompraService compraService = new CompraService();
 		compraService.crearCompra(usuarioDto, celularDto, 1);
+		compraService.imprimirCompras();
 
+		System.out.println();
 		System.out.println("****Crear Venta*******");
 		VentaService ventaService = new VentaService();
 		ventaService.crearVenta(usuarioDto, personaDto, celularDto, 2);
+		ventaService.imprimirVentas();
 
 	}
 }
